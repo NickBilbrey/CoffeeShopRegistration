a0eb1b7eb17c4f24fe345ea5903bca6f4d272bc3
Nicholas <NicholasBilbrey18@gmail.com>
2023-04-10 18:43:54 -0400

    initial commit

---
+++ Coffee_Shop_Lab/Controllers/CoffeeShopController.cs
@@ -0,0 +1,31 @@
+﻿using Coffee_Shop_Lab.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee_Shop_Lab.Controllers
+{
+    public class CoffeeShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrationView() 
+        {
+            return View();
+        }
+        public IActionResult HandleRegistration(Registration registration) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("RegistrationView");
+            }
+            return RedirectToAction("Summary", registration);
+        }
+
+        public IActionResult Summary(Registration registration) 
+        {
+            return View(registration);
+        }
+    }
+}
